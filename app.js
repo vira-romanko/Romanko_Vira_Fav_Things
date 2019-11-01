e2698e247b853c6c97463819b321f6497b01803c
Vira <vira.romanko99@gmail.com>
2019-11-01 17:26:49 -0400

    styling

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
@@ -13,6 +14,22 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use('/', require('./routes/index'));
 
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler goes here
+  app.use(function(err, req, res, next) {
+    //  only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
 app.listen(port, () => {
     console.log(`app is running on ${port}`);
 })
